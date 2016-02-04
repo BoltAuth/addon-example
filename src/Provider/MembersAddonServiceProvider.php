8c7374db50a99ce5fb2ffc5a3ed694e5bd34d453
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-04 09:04:56 +0100

    Inject add-on forms into the provider

--- src/Provider/MembersAddonServiceProvider.php
+++ src/Provider/MembersAddonServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Bolt\Extension\Europeana\MembersAddons\Provider;
 
+use Bolt\Extension\Bolt\Members\Form\Validator\Constraint\UniqueEmail;
+use Bolt\Extension\Europeana\MembersAddon\Form;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
@@ -17,6 +19,41 @@ class MembersAddonServiceProvider implements ServiceProviderInterface
      */
     public function register(Application $app)
     {
+        $app['members.forms'] = $app->share(
+            function ($app) {
+                $type = new \Pimple(
+                    [
+                        // @codingStandardsIgnoreStart
+                        'login'    => $app->share(function () use ($app) { return new Form\Type\LoginType(); }),
+                        'profile'  => $app->share(function () use ($app) { return new Form\Type\ProfileType(); }),
+                        'register' => $app->share(function () use ($app) { return new Form\Type\RegisterType($app['members.records']); }),
+                        // @codingStandardsIgnoreEnd
+                    ]
+                );
+                $entity = new \Pimple(
+                    [
+                        // @codingStandardsIgnoreStart
+                        'login'    => $app->share(function () use ($app) { return new Form\Entity\Login(); }),
+                        'profile'  => $app->share(function () use ($app) { return new Form\Entity\Profile($app['members.records']); }),
+                        'register' => $app->share(function () use ($app) { return new Form\Entity\Register(); }),
+                        // @codingStandardsIgnoreEnd
+                    ]
+                );
+                $constraint = new \Pimple(
+                    [
+                        // @codingStandardsIgnoreStart
+                        'email' => $app->share(function () use ($app) { return new UniqueEmail($app['members.records']); }),
+                        // @codingStandardsIgnoreEnd
+                    ]
+                );
+
+                return new \Pimple([
+                    'type'       => $type,
+                    'entity'     => $entity,
+                    'constraint' => $constraint,
+                ]);
+            }
+        );
     }
 
     /**
