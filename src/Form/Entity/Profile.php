da8aeec04bf3f461eef58af19310b0987eb59ce5
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-05 12:06:46 +0100

    Add additional fields to profile forms

--- src/Form/Entity/Profile.php
+++ src/Form/Entity/Profile.php
@@ -19,6 +19,27 @@ class Profile
     protected $email;
     /** @var string */
     protected $displayName;
+
+
+    /** @var string */
+    protected $twitterHandle;
+    /** @var string */
+    protected $website;
+    /** @var string */
+    protected $biography;
+    /** @var string */
+    protected $addressStreet;
+    /** @var string */
+    protected $addressStreetMeta;
+    /** @var string */
+    protected $addressCity;
+    /** @var string */
+    protected $addressState;
+    /** @var string */
+    protected $addressCountry;
+    /** @var string */
+    protected $phoneNumber;
+
     /** @var array */
     protected $meta;
     /** @var  Records */
@@ -107,5 +128,185 @@ class Profile
         }
 
         return $this->meta = $this->records->getAccountMetaAll($this->guid);
+    }
+
+    /**
+     * @return string
+     */
+    public function getTwitterHandle()
+    {
+        return $this->twitterHandle;
+    }
+
+    /**
+     * @param string $twitterHandle
+     *
+     * @return Profile
+     */
+    public function setTwitterHandle($twitterHandle)
+    {
+        $this->twitterHandle = $twitterHandle;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWebsite()
+    {
+        return $this->website;
+    }
+
+    /**
+     * @param string $website
+     *
+     * @return Profile
+     */
+    public function setWebsite($website)
+    {
+        $this->website = $website;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBiography()
+    {
+        return $this->biography;
+    }
+
+    /**
+     * @param string $biography
+     *
+     * @return Profile
+     */
+    public function setBiography($biography)
+    {
+        $this->biography = $biography;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddressStreet()
+    {
+        return $this->addressStreet;
+    }
+
+    /**
+     * @param string $addressStreet
+     *
+     * @return Profile
+     */
+    public function setAddressStreet($addressStreet)
+    {
+        $this->addressStreet = $addressStreet;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddressStreetMeta()
+    {
+        return $this->addressStreetMeta;
+    }
+
+    /**
+     * @param string $addressStreetMeta
+     *
+     * @return Profile
+     */
+    public function setAddressStreetMeta($addressStreetMeta)
+    {
+        $this->addressStreetMeta = $addressStreetMeta;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddressCity()
+    {
+        return $this->addressCity;
+    }
+
+    /**
+     * @param string $addressCity
+     *
+     * @return Profile
+     */
+    public function setAddressCity($addressCity)
+    {
+        $this->addressCity = $addressCity;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddressState()
+    {
+        return $this->addressState;
+    }
+
+    /**
+     * @param string $addressState
+     *
+     * @return Profile
+     */
+    public function setAddressState($addressState)
+    {
+        $this->addressState = $addressState;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddressCountry()
+    {
+        return $this->addressCountry;
+    }
+
+    /**
+     * @param string $addressCountry
+     *
+     * @return Profile
+     */
+    public function setAddressCountry($addressCountry)
+    {
+        $this->addressCountry = $addressCountry;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * @param string $phoneNumber
+     *
+     * @return Profile
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
     }
 }
--- src/Form/Type/ProfileType.php
+++ src/Form/Type/ProfileType.php
@@ -4,10 +4,7 @@ namespace Bolt\Extension\Europeana\MembersAddons\Form\Type;
 
 use Bolt\Translation\Translator as Trans;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -21,26 +18,80 @@ class ProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('displayname', TextType::class,   [
+            ->add('displayname', Type\TextType::class,   [
                 'label'       => Trans::__('Public name:'),
-                'data'        => $options['data']['displayname'],
+                'data'        => $this->getData($options, 'displayname'),
                 'constraints' => [
                     new Assert\NotBlank(),
                     new Assert\Length(['min' => 2]),
                 ],
             ])
-            ->add('email',       EmailType::class,   [
+            ->add('email',       Type\EmailType::class,   [
                 'label'       => Trans::__('Email:'),
-                'data'        => $options['data']['email'],
+                'data'        => $this->getData($options, 'email'),
                 'constraints' => new Assert\Email([
                     'message' => 'The address "{{ value }}" is not a valid email.',
                     'checkMX' => true,
                 ]),
             ])
-            ->add('plainPassword', RepeatedType::class, [
-                'type' => PasswordType::class,
+            ->add('plainPassword', Type\RepeatedType::class, [
+                'type' => Type\PasswordType::class,
                 'first_options'  => array('label' => 'Password'),
                 'second_options' => array('label' => 'Repeat Password'),
+            ])
+            ->add('website', Type\TextType::class,   [
+                'label'       => Trans::__('Website URL:'),
+                'data'        => $this->getData($options, 'website'),
+                'constraints' => [
+                ],
+            ])
+            ->add('twitter_handle', Type\TextType::class,   [
+                'label'       => Trans::__('Twitter Name:'),
+                'data'        => $this->getData($options, 'twitter_handle'),
+                'constraints' => [
+                ],
+            ])
+            ->add('biography', Type\TextareaType::class,   [
+                'label'       => Trans::__('Biography:'),
+                'data'        => $this->getData($options, 'biography'),
+                'constraints' => [
+                ],
+            ])
+            ->add('address_street', Type\TextType::class,   [
+                'label'       => Trans::__('Street Address:'),
+                'data'        => $this->getData($options, 'address_street'),
+                'constraints' => [
+                ],
+            ])
+            ->add('address_street_meta', Type\TextType::class,   [
+                'label'       => null,
+                'data'        => $this->getData($options, 'address_street_meta'),
+                'constraints' => [
+                ],
+            ])
+            ->add('address_city', Type\TextType::class,   [
+                'label'       => Trans::__('City:'),
+                'data'        => $this->getData($options, 'address_city'),
+                'constraints' => [
+                ],
+            ])
+            ->add('address_state', Type\TextType::class,   [
+                'label'       => Trans::__('Province / state / arrondissement:'),
+                'data'        => $this->getData($options, 'address_state'),
+                'constraints' => [
+                ],
+            ])
+            ->add('address_country', Type\TextType::class,   [
+                'label'       => Trans::__('Country:'),
+                'data'        => $this->getData($options, 'address_country'),
+                'constraints' => [
+                ],
+            ])
+            ->add('phone_number', Type\TextType::class,   [
+                'label'       => Trans::__('Phone number:'),
+                'data'        => $this->getData($options, 'phone_number'),
+                'constraints' => [
+                ],
             ])
             ->add('submit',      'submit', [
                 'label'   => Trans::__('Save & continue'),
@@ -50,5 +101,20 @@ class ProfileType extends AbstractType
     public function getName()
     {
         return 'profile';
+    }
+
+    /**
+     * @param array  $options
+     * @param string $field
+     *
+     * @return mixed|null
+     */
+    private function getData(array $options, $field)
+    {
+        if (!isset($options['data'])) {
+            return null;
+        }
+
+        return isset($options['data'][$field]) ? $options['data'][$field] : null;
     }
 }
