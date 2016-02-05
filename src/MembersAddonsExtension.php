f4dd16a3886ba6ed2c594477b363fd284ad3c6f3
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-05 17:23:56 +0100

    Subscribe to profile events

--- src/MembersAddonsExtension.php
+++ src/MembersAddonsExtension.php
@@ -2,9 +2,12 @@
 
 namespace Bolt\Extension\Europeana\MembersAddons;
 
+use Bolt\Extension\Bolt\Members\Event\MembersEvents;
+use Bolt\Extension\Bolt\Members\Event\MembersProfileEvent;
 use Bolt\Extension\Europeana\MembersAddons\Provider\MembersAddonsServiceProvider;
 use Bolt\Extension\SimpleExtension;
 use Silex\Application;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Members add-ons for Europeana.
@@ -32,6 +35,25 @@ class MembersAddonsExtension extends SimpleExtension
         return [
             'templates/profile' => ['position' => 'prepend']
         ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function subscribe(EventDispatcherInterface $dispatcher)
+    {
+        $dispatcher->addListener(MembersEvents::MEMBER_PROFILE_SAVE, [$this, 'onProfileSave']);
+    }
+
+    /**
+     * Tell Members what fields we want to persist.
+     *
+     * @param MembersProfileEvent $event
+     */
+    public function onProfileSave(MembersProfileEvent $event)
+    {
+        $config = $this->getConfig();
+        $event->setMetaFields($config['meta_fields']['profile']);
     }
 
     /**
