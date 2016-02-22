21488c65801b4ed1a3bf6c50521c3a3b1a8cacfb
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-22 15:43:46 +0100

    Only extend required components

--- src/Provider/MembersAddonsServiceProvider.php
+++ src/Provider/MembersAddonsServiceProvider.php
@@ -46,43 +46,21 @@ class MembersAddonsServiceProvider implements ServiceProviderInterface
      */
     public function onRequest(Application $app)
     {
-        $app['members.form.components'] = $app->share(
-            function ($app) {
-                $type = new \Pimple(
-                    [
-                        // @codingStandardsIgnoreStart
-                        'associate' => $app->share(function () use ($app) { return new Form\Type\AssociateType($app['members.config']); }),
-                        'login'     => $app->share(function () use ($app) { return new Form\Type\LoginType($app['members.config']); }),
-                        'logout'    => $app->share(function () use ($app) { return new Form\Type\LogoutType($app['members.config']); }),
-                        'profile'   => $app->share(function () use ($app) { return new Form\Type\ProfileType($app['members.config']); }),
-                        'register'  => $app->share(function () use ($app) { return new Form\Type\RegisterType($app['members.config'], $app['members.records']); }),
-                        // @codingStandardsIgnoreEnd
-                    ]
+        $app['members.form.components'] = $app->extend(
+            'members.form.components',
+            function ($components, $app) {
+                $components['type']['profile'] = $app->share(
+                    function () use ($app) {
+                        return new Form\Type\ProfileType($app['members.config']);
+                    }
                 );
-                $entity = new \Pimple(
-                    [
-                        // @codingStandardsIgnoreStart
-                        'associate' => $app->share(function () use ($app) { return new Form\Entity\Associate(); }),
-                        'login'     => $app->share(function () use ($app) { return new Form\Entity\Login(); }),
-                        'logout'    => $app->share(function () use ($app) { return new Form\Entity\Logout(); }),
-                        'profile'   => $app->share(function () use ($app) { return new Form\Entity\Profile($app['members.records']); }),
-                        'register'  => $app->share(function () use ($app) { return new Form\Entity\Register(); }),
-                        // @codingStandardsIgnoreEnd
-                    ]
-                );
-                $constraint = new \Pimple(
-                    [
-                        // @codingStandardsIgnoreStart
-                        'email' => $app->share(function () use ($app) { return new UniqueEmail($app['members.records']); }),
-                        // @codingStandardsIgnoreEnd
-                    ]
+                $components['entity']['profile'] = $app->share(
+                    function () use ($app) {
+                        return new Form\Entity\Profile($app['members.records']);
+                    }
                 );
 
-                return new \Pimple([
-                    'type'       => $type,
-                    'entity'     => $entity,
-                    'constraint' => $constraint,
-                ]);
+                return $components;
             }
         );
     }
